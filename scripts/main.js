48e39a91af863ca88c56df4fca3b39888007c239
PiotrBerebecki <petebere@gmail.com>
2016-09-09 12:52:52 +0100

    Add page grid and header logo

---
+++ scripts/main.js
@@ -0,0 +1,74 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// ----------------
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
+    );
+  }
+});
+
+// ----------------
+var Header = React.createClass({
+  render: function() {
+    const { tagline } = this.props;
+    return (
+      <header className="top">
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline"><span>{tagline}</span></h3>
+      </header>
+    );
+  }
+});
+
+// ----------------
+var Order = React.createClass({
+  render: function() {
+    return (
+      <p>Order</p>
+    );
+  }
+});
+
+// ----------------
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <p>Inventory</p>
+    );
+  }
+});
+
+// ----------------
+var StorePicker = React.createClass({
+  render: function() {
+    return (
+     <form className="store-selector">
+      <h2>Please Enter A Store</h2>
+      <input type="text" ref="storeId" required/>
+      <input type="submit"/>
+     </form>
+    );
+  }
+});
+
+// ----------------
+ReactDOM.render(
+  <App/>,
+  document.getElementById('main')
+);
