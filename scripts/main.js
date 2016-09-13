fe7706ff9b2bfef855fd170071442cf93747e6c6
PiotrBerebecki <petebere@gmail.com>
2016-09-13 14:23:54 +0100

    Add propTypes validation

--- scripts/main.js
+++ scripts/main.js
@@ -112,6 +112,9 @@ var Header = React.createClass({
         <h3 className="tagline"><span>{tagline}</span></h3>
       </header>
     );
+  },
+  propTypes: {
+    tagline: React.PropTypes.string.isRequired
   }
 });
 
@@ -206,13 +209,18 @@ var Order = React.createClass({
         
       </div>
     );
+  },
+  propTypes: {
+    fishes: React.PropTypes.object.isRequired,
+    order: React.PropTypes.object.isRequired,
+    removeFromOrder: React.PropTypes.func.isRequired
   }
 });
 
 // ----------------
 var Inventory = React.createClass({
   renderInventory: function(key) {
-    var { linkState, removeFish } = this.props;
+    var { linkState } = this.props;
     return (
       <div className="fish-edit" key={key}>
         <input type="text" valueLink={linkState(`fishes.${key}.name`)}/>
@@ -236,6 +244,13 @@ var Inventory = React.createClass({
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     );
+  },
+  propTypes: {
+    addFish: React.PropTypes.func.isRequired,
+    removeFish: React.PropTypes.func.isRequired,
+    loadSamples: React.PropTypes.func.isRequired,
+    linkState: React.PropTypes.func.isRequired,
+    fishes: React.PropTypes.object.isRequired
   }
 });
 
